204695ad2af9999280594f629eef2bc0239ad216
Neeraja Gogte <neerajagogte@Neerajas-MacBook-Air.local>
2021-04-04 20:04:52 -0700

    M07

---
+++ m07/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bag Store</title>
+
+	<? include "parts/meta.php" ?>
+
+</head>
+<body>
+	
+	<? include "parts/navbar.php" ?>
+
+	<div class="view-window hero" style="background-image: url(../img/hike.jpg);">
+		<h1>Bags</h1>
+	</div>
+
+	<div class="container pad" id="introduction">
+		<div class="card card-soft card-light card-flat">
+			<div class="grid">
+				<div class="col-xs-12 col-md-6 center-child">
+					<div style="max-width: 75%;">
+						<h2>The Bag Store</h2>
+						<p>Looking for bags? <span> Lorem ipsum, dolor sit amet, consectetur adipisicing elit. </span> <span>Perferendis rem dolores et ab minima atque a quam nobis quos cumque maxime earum itaque molestias reiciendis, architecto, </span><span>saepe debitis repellat quisquam enim eum natus, eaque. Voluptates impedit officia, perferendis deleniti maiores veritatis, eum porro nostrum numquam incidunt suscipit </span><span>distinctio esse molestias quod ea asperiores ut debitis, voluptas ullam amet. Dolorum, nesciunt.</span></p>
+					</div>
+				</div>
+				<div class="col-xs-12 col-md-6">
+					<img src="https://via.placeholder.com/600/f88/fff?text=Apples" class="image-cover">
+					<!--google images apples-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr class="spacer large">
+
+	<div class="container pad" id="categories">
+		<h2>Categories</h2>
+		<div class="grid gap xs-small md-large">
+			<div class="col-xs-6 col-md-3">
+				<figure>
+					<div class="image-circle">
+						<img src="https://via.placeholder.com/400/f88/fff?text=Bags" alt="Bags">
+					</div>
+					<figcaption>Bags</figcaption>
+				</figure>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<figure>
+					<div class="image-circle">
+						<img src="https://via.placeholder.com/400/f88/fff?text=Travel" alt="Travel">
+					</div>
+					<figcaption>Travel</figcaption>
+				</figure>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<figure>
+					<div class="image-circle">
+						<img src="https://via.placeholder.com/400/f88/fff?text=Hiking" alt="Hiking">
+					</div>
+					<figcaption>Hiking</figcaption>
+				</figure>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<figure>
+					<div class="image-circle">
+						<img src="https://via.placeholder.com/400/f88/fff?text=Casual" alt="Casual">
+					</div>
+					<figcaption>Casual</figcaption>
+				</figure>
+			</div>
+		</div>
+	</div>
+		
+	<hr class="spacer large">
+	
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
